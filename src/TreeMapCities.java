0f6dfcfa0d1c62bdc5ed1943cb6282527edc1ab8
Michał <michgluacad7@gmail.com>
2023-10-29 12:34:38 +0000

    dodanie 3 projektu

---
+++ src/TreeMapCities.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class TreeMapCities {
+    public static void main(String[] args) {
+        printing();
+    }
+        public static void printing(){
+
+        // Tworzenie mapy TreeMap z nazwami miast i ilościami ludności
+        TreeMap<String, Integer> cityPopulationMap = new TreeMap<>();
+
+        // Dodawanie danych do mapy
+        cityPopulationMap.put("Warszawa", 1861975);
+        cityPopulationMap.put("Kraków", 803352);
+        cityPopulationMap.put("Wrocław", 673923);
+        cityPopulationMap.put("Poznań", 534813);
+        cityPopulationMap.put("Gdańsk", 470907);
+
+        // Tworzenie komparatora do sortowania mapy według ilości ludności
+        Comparator<String> populationComparator = new Comparator<String>() {
+            public int compare(String city1, String city2) {
+                int population1 = cityPopulationMap.get(city1);
+                int population2 = cityPopulationMap.get(city2);
+                return population1 - population2;
+            }
+        };
+
+        // Tworzenie posortowanej mapy za pomocą komparatora
+        TreeMap<String, Integer> sortedCityPopulationMap = new TreeMap<>(populationComparator);
+        sortedCityPopulationMap.putAll(cityPopulationMap);
+
+        // Wyświetlanie posortowanej mapy
+        for (Map.Entry<String, Integer> entry : sortedCityPopulationMap.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue() + " ludności");
+        }
+    }
+}
